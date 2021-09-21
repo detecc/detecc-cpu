9c046b2aad8339cfc3707f09f1048a685b3f8e01
Ergjan Jaha <ergjan.jaha@e-identitet.se>
2021-09-21 17:18:05 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/detecc/detecc-cpu
+
+go 1.16
+
+require github.com/detecc/detecctor v0.0.0-20210921145657-ca04f23f59ec
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/detecc/detecctor/plugin"
+	"github.com/detecc/detecctor/shared"
+	"log"
+	"strings"
+)
+
+func init() {
+	example := &Example{}
+	plugin.Register(example.GetCmdName(), example)
+}
+
+type Example struct {
+	plugin.Handler
+}
+
+func (e Example) GetCmdName() string {
+	return "/example"
+}
+
+func (e Example) Response(payload shared.Payload) shared.Reply {
+	log.Println(payload)
+	content := payload.Data.([]interface{})
+	content2 := make([]string, len(content))
+	for i, v := range content {
+		content2[i] = fmt.Sprint(v)
+	}
+
+	return shared.Reply{
+		ChatId:    904544573,
+		ReplyType: shared.TypeMessage,
+		Content:   strings.Join(content2, ", "),
+	}
+}
+
+func (e Example) Execute(args ...string) ([]shared.Payload , error) {
+	log.Println(args)
+	return []shared.Payload{
+		shared.Payload{
+			Id:             "0",
+			ServiceNodeKey: "a7309be127cf7g9127309",
+			Data:           []string{"test", "test2"},
+			Command:        e.GetCmdName(),
+			Success:        true,
+			Error:          "",
+		},
+	}, nil
+}
